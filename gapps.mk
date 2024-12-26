4a9f93e375fc1bde674f5c802e971d56b6e4528c
V3NCO <75168136+V3NCO@users.noreply.github.com>
2024-12-26 23:03:27 +0100

    Update gapps.mk

--- gapps.mk
+++ gapps.mk
@@ -24,7 +24,7 @@ PRODUCT_COPY_FILES += \
 	device/phh/treble/empty-permission.xml:system/etc/permissions/com.google.android.camera.experimental2017.xml
 
 PRODUCT_PACKAGE_OVERLAYS += device/phh/treble/overlay-gapps
-GAPPS_VARIANT := pico
+GAPPS_VARIANT := super
 DONT_DEXPREOPT_PREBUILTS := true
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
